898aeee401332c2f77ea1777a72c3ab8e3749fe2
“Aditya” <adityaganeshan@gmail.com>
2013-08-21 21:36:48 +0530

    basic model for upload made

--- apps/lectut/models.py
+++ apps/lectut/models.py
@@ -3,5 +3,19 @@ from core import models
 # Create your models here.
 
 
-class Uploader(models.Model):
+class FileUpload(models.Model):
+   file_id = models.BigIntegerField(primary_key=True)
+   file_location = models.CharField(max_length=450)
+   topic = models.CharField(max_length=450)
+   timestamp = models.DateTimeField()
+   year = models.CharField(max_length=45, blank=True)
+   file_type_list = (
+       ('EP', 'Exampaper'),
+       ('Le', 'Lecture'),
+       ('So', 'Solution'),
+       )
+   file_type = modelsCharField(max_lenght=1, choices=file_type_list)
+   faculty = models.ForeignKey(Faculty)
+   course = models.ForeignKey(Course)
+
 
