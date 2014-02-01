d476ebff5555268642c29c578221d37f548409b6
Akshesh <aksheshdoshi@gmail.com>
2014-02-02 00:45:41 +0530

    send data through ajax

--- apps/facapp/views.py
+++ apps/facapp/views.py
@@ -4,10 +4,15 @@ from django.http import HttpResponse, HttpResponseRedirect
 from facapp.models import *
 from django.shortcuts import render
 from facapp import data
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def home(request):
-  if 'submit' in request.POST:
-    return HttpResponse("hello there")
+  print "yess"
+  if 'ckedit' in request.POST:
+    show = request.POST["ckedit"]
+    print "here"
+    return HttpResponse(show)
 #   return HttpResponse(data.titles)
   return render(request, 'facapp/home.html')
 
