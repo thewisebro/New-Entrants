a84d71c185f87d0e9d58ca52b42c6a98c19dcc11
Jagannadh <shanmuk.mir@gmail.com>
2014-08-05 21:42:26 +0530

    News: Cron script updated

--- apps/news/scripts/scrap_n_load.py
+++ apps/news/scripts/scrap_n_load.py
@@ -2,7 +2,6 @@
 
 import os, sys
 #sys.path.append(os.path.join(os.path.abspath("../../../..")))
-
 #from django.core.management import setup_environ
 #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "channeli.settings")
 from django.conf import settings
@@ -16,41 +15,16 @@ def load_data():
   os.chdir(settings.NEWS_IMAGES_ROOT)
   #print(os.getcwd())
   try:
-    #ht(path, 'national')
-    #ht(path, 'sports')
-
-    """
-    hindu.Entertainment(path)
-    toi.Entertainment(path)
-    ie.Entertainment(path)
-    msn.Entertainment(path)
-    bbc.Entertainment(path)
-    """
-
     bbc.selectAll(path)
     hindu.selectAll(path)
     toi.selectAll(path)
     ie.selectAll(path)
     msn.selectAll(path)
     yahoo.selectAll(path)
-
-    """
-    hindu.Technology(path)
-    toi.Technology(path)
-    ie.Technology(path)
-    bbc.Technology(path)
-    hindu.Education(path)
-    toi.Education(path)
-    ie.Education(path)
-    hindu.Health(path)
-    toi.Health(path)
-    ie.Health(path)
-    bbc.Health(path)
-    """
-
-    return HttpResponse("Task Completed!!")
+    return True
   except Exception as e:
-    return HttpResponse(e)
+    print e
+    return False
 
 
 if __name__ == '__main__':
