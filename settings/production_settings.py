57aba83d008f72153d5f703bafad81fabee3d370
Shubham Jain <sj.iitr@gmail.com>
2014-08-21 05:01:36 +0530

    settings: fix production settings

--- settings/production_settings.py
+++ settings/production_settings.py
@@ -4,6 +4,12 @@ PRODUCTION = True
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
+ALLOWED_HOSTS = [
+  '192.168.121.5',
+  '.channeli.in',
+  '.iitr.ernet.in',
+]
+
 try:
   from production.settings import *
 except ImportError:
