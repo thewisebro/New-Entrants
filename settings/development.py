d6527242a41518e1501283168bc3000a21ff4e10
Jagannadh <shanmuk.mir@gmail.com>
2014-09-08 20:39:39 +0530

    News: Elastic search backend attached to django-haystack module

--- settings/development.py
+++ settings/development.py
@@ -377,20 +377,24 @@ LOGGING = {
 
 HAYSTACK_CONNECTIONS = {
     'default': {
-      'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
-      'URL': 'http://192.168.121.5:8983/solr/',
+      'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+      'URL': 'http://192.168.121.5:9200/',
+      'INDEX_NAME': 'channeli',
       'INCLUDE_SPELLING': True,
       # ...or for multicore...
       # 'URL': 'http://127.0.0.1:8983/solr/mysite',
     },
     'autocomplete': {
-      'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
-      'URL': 'http://192.168.121.5:8983/solr/',
+      'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+      'URL': 'http://192.168.121.5:9200/',
+      'INDEX_NAME': 'channeli',
       'INCLUDE_SPELLING': True,
       # ...or for multicore...
       # 'URL': 'http://127.0.0.1:8983/solr/mysite',
     }
 }
+
+HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
 
 from admin_settings import *
 
