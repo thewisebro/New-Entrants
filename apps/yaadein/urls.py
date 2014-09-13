3a3b9dd34b712443250a41c97964edf07b299e7b
vaibhav <gvaibhavraj2@gmail.com>
2014-09-13 12:37:26 +0530

    base template working

---
+++ apps/yaadein/urls.py
@@ -0,0 +1,9 @@
+
+from django.conf.urls import url
+
+from yaadein import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    ]
+
--- apps/yaadein/views.py
+++ apps/yaadein/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from datetime import date, datetime, timedelta
 from django.template import RequestContext
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 
 
-
- @login_required
 def index(request):
-  user=request.user
+  return render_to_response('yaadein/base.html',{},context_instance=RequestContext(request))
 
