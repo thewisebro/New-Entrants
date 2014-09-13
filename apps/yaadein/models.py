5ce7bfba78047d9535351eac64eed9e2be047125
vaibhav <gvaibhavraj2@gmail.com>
2014-09-13 13:16:58 +0530

    models updated

--- apps/yaadein/models.py
+++ apps/yaadein/models.py
@@ -1,2 +1,18 @@
 from core import models
 
+
+class post(models.Model):
+  text-content = models.TextField(blank=True)
+  tags = models.ManyToManyField('FeedTag', blank=True, null=True)
+  images=models.ManyToManyField('Image')
+  post-date = models.DateTimeField(auto_now=True)
+
+class FeedTag(models.Model):
+  value = models.CharField(max_length=200)
+  def __str__(self):
+    return self.value
+
+class Image(models.Model):
+
+
+
