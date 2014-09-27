2ccb1b96583bc022cbac313255bf42edc4ea0841
vaibhav <gvaibhavraj2@gmail.com>
2014-09-27 15:39:23 +0530

    tag added

--- apps/yaadein/models.py
+++ apps/yaadein/models.py
@@ -3,14 +3,17 @@ from core import models
 
 class post(models.Model):
   text_content = models.CharField(max_length=200)
-# tags = models.ManyToManyField('FeedTag', blank=True, null=True)
+  tags = models.ManyToManyField('FeedTag', blank=True, null=True)
  # images=models.ManyToManyField('Image')
   post_date = models.DateTimeField(auto_now=True)
+  def __unicode__(self):
+    return self.text_content
 
-#class FeedTag(models.Model):
-# value = models.CharField(max_length=200)
-# def __str__(self):
-#   return self.value
+
+class FeedTag(models.Model):
+   value = models.CharField(max_length=200)
+   def __str__(self):
+     return self.value
 
 
 
