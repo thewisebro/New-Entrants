8064815e904623a9bb2f8151b8e6ecf9db547568
Himanshu <himanshujariyal@gmail.com>
2015-01-26 02:03:13 +0530

    myCourses

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,36 @@
  */
 angular.module('lectutApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+
+    //current user details
+    $scope.userdetails = {
+     username:"Himanshu Jariyal",
+     enrollmentNo:"12114030",
+     branch:"CSE"  
+    };
+   
+    //course name of current user
+    $scope.courses = [
+    {
+      name:"Electronics and Communication",
+      id:"EC-101"
+    },
+    {
+      name:"Physics",
+      id:"CC-301"
+    }, 
+    {
+      name:"Business Managemnet",
+      id:"ME-401"
+    }, 
+    {
+      name:"Fine arts and music",
+      id:"REC-101"
+    },
+    {
+      name:"Hardware Lab",
+      id:"WE-161"
+    }
     ];
   });
+
