3f27fd5786fac46ef3673f4379a5e311924cfea1
Himanshu <himanshujariyal@gmail.com>
2015-01-25 19:29:44 +0530

    yo angular

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name lectutApp
+ * @description
+ * # lectutApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('lectutApp', [
+    'ngAnimate'
+  ]);
