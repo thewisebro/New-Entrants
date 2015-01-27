c31d45db70002acdca46a1b50527d75853b18252
Jagannadh <shanmuk.mir@gmail.com>
2015-01-27 19:20:11 +0530

    Yaadein: post status, yaadein_user status fields added

--- apps/yaadein/models.py
+++ apps/yaadein/models.py
@@ -8,28 +8,33 @@ from api import model_constants as MC
 from taggit_autocomplete.managers import TaggableManager
 
 class YaadeinUser(models.Model):
-   user = models.OneToOneField(User)
-   coverpic = models.FileField(upload_to='yaadein/coverpic/', null=True, blank=True)
-   def __unicode__(self):
-     return self.user.name
+  """
+    This inherited child of 'User' model is to bind all those
+    user attributes which are specific to Yaadein app
+  """
+  user = models.OneToOneField(User)
+  coverpic = models.FileField(upload_to='yaadein/coverpic/', null=True, blank=True)
+  status = models.CharField(choices=YC.YAADEIN_USER_STATUS, default='A')  # Default is 'Active'
+  def __unicode__(self):
+    return self.user.name
 
 class Post(models.Model):
   text_content = models.CharField(max_length=200)
   tags = TaggableManager()
-#  images=models.ManyToManyField('Image')
+  #images=models.ManyToManyField('Image')
   post_date = models.DateTimeField(auto_now=True)
   user_tags = models.ManyToManyField(Student, related_name="tagged_user")
   owner = models.ForeignKey(Student, related_name="post_owner")
   wall_user = models.ForeignKey(Student, related_name="post_wall_user")
+  status = models.CharField(choices=YC.POST_STATUS, default='A') # Default is 'Active'
   def __unicode__(self):
     return self.text_content
 
 class PostImage(models.Model):
-      image = models.FileField(upload_to='yaadein/')
-      post = models.ForeignKey(Post)
-
-      def __unicode__(self):
-        return self.image.url
+  image = models.FileField(upload_to='yaadein/')
+  post = models.ForeignKey(Post)
+  def __unicode__(self):
+    return self.image.url
 
 
 
