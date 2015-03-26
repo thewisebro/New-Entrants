448d87096441df12c6511e8fa3d74042ea39bf40
namit <namit.ohri@gmail.com>
2015-03-26 16:36:52 +0530

    namit@groups:edit form design change

--- settings/development.py
+++ settings/development.py
@@ -47,7 +47,7 @@ MANAGERS = ADMINS
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-    'NAME': 'fish',                   # Or path to database file if using sqlite3.
+    'NAME': 'nci',                   # Or path to database file if using sqlite3.
                                           # The following settings are not used with sqlite3:
     'USER': 'channeli',
     'PASSWORD': 'channeli',
