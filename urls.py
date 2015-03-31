1db9b73522eddb5b303c3cc271e9307e387e0fd5
vaibhav <gvaibhavraj2@gmail.com>
2015-03-31 21:31:07 +0530

    Yaadein:Urls change

--- urls.py
+++ urls.py
@@ -26,7 +26,7 @@ actual_urlpatterns = patterns('',
   url(r'^media/(.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
   url(r'^newsmedia/(.*)$', 'django.views.static.serve', {'document_root': settings.NEWS_MEDIA_ROOT}),
   url(r'^songsmedia/(.*)$', 'django.views.static.serve', {'document_root': settings.JUKEBOX_MEDIA_ROOT}),
-  url(r'^yaadein/', include('yaadein.urls')),
+  url(r'^yaadein_api/', include('yaadein.urls')),
 )
 
 urlpatterns = patterns('',
