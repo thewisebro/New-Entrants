3796af31e0ab5bc998d15fd452575c7c2334736a
Akshesh <aksheshdoshi@gmail.com>
2015-04-01 19:56:37 +0530

    Facapp_port: Add 'pika' to install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -77,6 +77,7 @@ pip install mutagen
 pip install reportlab
 pip install python-dateutil
 pip install simplejson
+pip install pika
 
 # fuzzy wuzzy
 pip install -e git+https://github.com/seatgeek/fuzzywuzzy.git#egg=fuzzywuzzy
