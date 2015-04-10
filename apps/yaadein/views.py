4f9bcaba48bcb29591c9ddca2924546fc4bacee0
Pankaj Gudlani <pankaj.gudlani@gmail.com>
2015-04-10 21:05:09 +0530

    yaadein: temp: added channeli.in in notif

--- apps/yaadein/views.py
+++ apps/yaadein/views.py
@@ -475,7 +475,7 @@ def invite(request):
         sent_users.append(student_related.user.username)
     if len(user_tagged)>0: 
       Notification.save_notification(app,notif_msg1,url,tagged_users,student)
-      requests.post("/notifications/add/",data={'channeli':True,'app':app,'text':notif_msg1,'users':sent_users,'url':url})
+      requests.post("https://channeli.in/notifications/add/",data={'channeli':True,'app':app,'text':notif_msg1,'users':sent_users,'url':url})
       return HttpResponse("True")
     else:
       return HttpResponse("False")
