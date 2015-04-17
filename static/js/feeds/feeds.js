7271169c8c3ef8a41fadb0c91369ef09736686b3
Shubham Jain <sj.iitr@gmail.com>
2015-04-17 19:49:26 +0530

    feeds: customized feeds

--- static/js/feeds/feeds.js
+++ static/js/feeds/feeds.js
@@ -19,6 +19,15 @@ function load_feeds_page(){
   show_default_right_column();
 }
 
+function feeds_on_login_logout() {
+  feeds = Array();
+  more_feeds = true;
+  load_feeds_page();
+}
+
+$(document).on("login", feeds_on_login_logout);
+$(document).on("logout", feeds_on_login_logout);
+
 function update_feeds(action,number){
   if(action == 'previous'){
     if(feeds.length === 0)
