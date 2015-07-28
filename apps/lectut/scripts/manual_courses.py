af68931025e9c6d55511a59523d853b5733367e6
Harshit <harshit.chopra.19@gmail.com>
2015-07-28 18:54:02 +0530

    Made scripts more generic

--- apps/lectut/scripts/manual_courses.py
+++ apps/lectut/scripts/manual_courses.py
@@ -3,7 +3,7 @@
     the database '''
 
 from nucleus.models import Course
-course_list = ['HS-001BA','HS-001','CE-104WA','BT-204','CE-453','ICE-201','IHS-30','MT-332','EC-221','BT-202GE','ES-407HY','CE-202','IPH-102','EC-622','PEC-1','HS-001AB','IHY-101','EC-324','CH-001EN','IC-102','MA-207','IMA-17','CE-207','AS-103S','ES-201CI','CE-205','CE-206','MA-001QW','EC-102KR']
+course_list = ['HS-001BA','HS-001','CE-104WA','BT-204','CE-453','ICE-201','IHS-30','MT-332','EC-221','BT-202GE','ES-407HY','CE-202','IPH-102','EC-622','PEC-1','HS-001AB','IHY-101','EC-324','CH-001EN','IC-102','MA-207','IMA-17','CE-207','AS-103S','ES-201CI','CE-205','CE-206','MA-001QW','EC-102KR','CH-404','HSN-01','IIEQ-02','EC-431','VA-503','PH-102','EE-429','CS-252','HSN-01','PH-00S','DS-102']
 
 success = 0
 course_len = len(course_list)
