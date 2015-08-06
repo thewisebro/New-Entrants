ab021a07183a4c390944ddb4e9d27f9f789b689a
Ayush <tiwari.ayush2412@gmail.com>
2015-08-06 15:42:55 +0530

    Placement: Added CGPA upload script for PG 1st and 2nd semester

--- apps/placement/scripts/CGPA_Load_PG.py
+++ apps/placement/scripts/CGPA_Load_PG.py
@@ -2,9 +2,6 @@ from nucleus.models import *
 from placement.models import *
 
 from upload_xls import ExcelParser
-# XXX Beware of the usage of ExcelParser from upload_xls file
-# The keys in row might change , for ex - in case of UG upload,
-# keys are different, and in case of PG, its different
 def previous_sem(curr_sem):
   ''' curr_sem = "UG20" -> previous_sem = "UG11" '''
   try:
@@ -32,27 +29,39 @@ def previous_sem(curr_sem):
     if curr_sem == "0":
       return curr_sem
 
-rows = ExcelParser('CGPA_2015.xlsx')
+rows = ExcelParser('SGPA_PG.xlsx')
 
+failed = []
 for l in rows:
-  student = Student.objects.get(user__username=l['roll'])
   try:
-   edu = EducationalDetails.objects.get(student=student,course=previous_sem(student.semester))
+    student = Student.objects.get(user__username=l['roll'])
+  except Student.DoesNotExist:
+    failed.append(l)
+    continue
+#import ipdb; ipdb.set_trace();
+  if int(l['sem_no'])==0:
+    course='PG10'
+    year = '2014'
+  if int(l['sem_no'])==1:
+    course='PG11'
+    year='2015'
+  try:
+   edu = EducationalDetails.objects.get(student=student,course=course)
    edu.sgpa = l['sgpa']
    edu.cgpa = l['cgpa']
    print student
    edu.save()
   except EducationalDetails.DoesNotExist:
-   edu = EducationalDetails.objects.create(student=student, course=previous_sem(student.semester), sgpa=l['sgpa'], cgpa=l['cgpa'], year=2015, institution="Indian Institute of Technology, Roorkee", discipline=student.branch.code)
+   edu = EducationalDetails.objects.create(student=student, course=course, sgpa=l['sgpa'], cgpa=l['cgpa'], year=year, institution="Indian Institute of Technology, Roorkee", discipline=student.branch.code)
    edu.save()
   except:
-   edu_lst = EducationalDetails.objects.filter(student=student,course=previous_sem(student.semester))
+   edu_lst = EducationalDetails.objects.filter(student=student,course=course)
    for edu in edu_lst:
      edu.sgpa = l['sgpa']
      edu.cgpa = l['cgpa']
      print student
      edu.save()
-    
+
 #print previous_sem("UG10")
 #print previous_sem("UG11")
 #print previous_sem("UG20")
