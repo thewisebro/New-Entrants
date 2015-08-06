e026e913771ad06ac33e3bd0bd1e42cb63620d5c
Kartik Patel <kartikpatel1995@gmail.com>
2015-08-07 00:56:38 +0530

    Placement: Minor change in script added

--- apps/placement/scripts/CGPA_Load_PG.py
+++ apps/placement/scripts/CGPA_Load_PG.py
@@ -1,33 +1,7 @@
 from nucleus.models import *
 from placement.models import *
-
+from placement.utils import *
 from upload_xls import ExcelParser
-def previous_sem(curr_sem):
-  ''' curr_sem = "UG20" -> previous_sem = "UG11" '''
-  try:
-    disp = curr_sem[:2]
-    if disp not in ["UG","PG"]:
-        disp = "PHD"
-    current_sem_code = curr_sem[-2:]
-    current_sem_code_year = current_sem_code[0]
-    current_sem_code_sem = current_sem_code[1]
-    try:
-      if current_sem_code_sem == "1":
-        prev_sem_code_sem = "0"
-        prev_sem_code_year = current_sem_code_year
-      else:
-        prev_sem_code_year = str(int(current_sem_code_year)-1)
-        prev_sem_code_sem = "1"
-      if prev_sem_code_year == "0":
-        return ""
-      return disp+prev_sem_code_year+prev_sem_code_sem
-    except ValueError:
-      return curr_sem
-  except:
-    if curr_sem == "":
-      return curr_sem
-    if curr_sem == "0":
-      return curr_sem
 
 rows = ExcelParser('SGPA_PG.xlsx')
 
@@ -38,7 +12,6 @@ for l in rows:
   except Student.DoesNotExist:
     failed.append(l)
     continue
-#import ipdb; ipdb.set_trace();
   if int(l['sem_no'])==0:
     course='PG10'
     year = '2014'
