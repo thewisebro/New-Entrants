d11dd4a9c931ddb16f2e02a0b0e79ecd479d6f9c
Kartik Patel <kartikpatel1995@gmail.com>
2015-08-12 11:01:23 +0530

    Internship: Priority enabled

--- apps/internship/views_student.py
+++ apps/internship/views_student.py
@@ -246,7 +246,7 @@ def submitted_resume(request, company_id) :
 def set_priority(request):
   student = request.user.student
   internship_person = InternshipPerson.objects.get(student=student)
-  companies_applied = CompanyApplicationMap.objects.filter(student=internship_person, company__pk__in = [366, 367, 368])
+  companies_applied = CompanyApplicationMap.objects.filter(student=internship_person, company__pk__in = [366, 367, 368, 371])
   message=""
   for company in companies_applied:
     company_priority = CompanyPriority.objects.get_or_create(student=student,company=company.company)
