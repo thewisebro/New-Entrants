9bef29bca00ed9f6f7dd243bf447bae6d9c42aed
apps <apps>
2015-08-20 21:07:45 +0530

    Placement: refactored placement status change script

--- apps/placement/scripts/change_placement_status.py
+++ apps/placement/scripts/change_placement_status.py
@@ -1,15 +1,16 @@
 from nucleus.models import *
-from placemnet.models import *
+from placement.models import *
 from placement.models import PlacementPerson
 
 PlacementPerson.objects.all().update(status='CLS')
-btech_students = Student.objects.filter(branch__degree="B.Tech.",semester="UG40")
-idd_students = Student.objects.filter(branch__degree="IDD",semester__in=["UG40","UG50"])
-barch_students = Student.objects.filter(branch__degree="B.Arch.",semester__in=["UG40","UG50"])
-imt_students = Student.objects.filter(branch__degree="IMT",semester__in=["UG40","UG50"])
-imsc_students = Student.objects.filter(branch__degree="IMSc",semester__in=["UG40","UG50"])
+btech_students = Student.objects.filter(branch__degree="B.Tech.",semester="UG40", passout_year=None)
+idd_students = Student.objects.filter(branch__degree="IDD",semester="UG50", passout_year=None)
+barch_students = Student.objects.filter(branch__degree="B.Arch.",semester="UG50",  passout_year=None)
+imt_students = Student.objects.filter(branch__degree="IMT",semester="UG50",  passout_year=None)
+imsc_students = Student.objects.filter(branch__degree="IMSc",semester="UG50",  passout_year=None)
 ## Re run this MTech part
-mtech_students = Student.objects.filter(branch__degree="M.Tech.", semester="PG20")
+mtech_students = Student.objects.filter(branch__degree="M.Tech.", semester="PG20",  passout_year=None)
+mca_students = Student.objects.filter(branch__degree='MCA', semester='PG40',  passout_year=None)
 
 success_btech = 0
 failure_btech = 0
@@ -84,7 +85,20 @@ for i in mtech_students:
     plc_person.status='OPN'
     plc_person.save()
     success_mtech += 1
-  except:
+   except:
     print str(i.user.username)+" Failure"
     failure_mtech += 1
 print "MTech Done"
+
+success_mca = 0
+failure_mca = 0
+for i in mca_students:
+  try:
+    plc_person, created = PlacementPerson.objects.get_or_create(student=i, status='CLS')
+    plc_person.status='OPN'
+    plc_person.save()
+    success_mca += 1
+  except:
+    print str(i.user.username)+" Failure"
+    failure_mca += 1
+print "MCA Done"
