8b780dca0088044addb98d4fdb773a68be7fc0ed
vaibhav <gvaibhavraj2@gmail.com>
2015-08-22 07:52:49 +0530

    Gate:FormApp

---
+++ apps/gate/constants.py
@@ -0,0 +1,34 @@
+#type of scholarship
+
+WEEKS= (
+    ('WEEK1','January 30-31, 2016'),
+    ('WEEK2','February 06-07, 2016'),
+    )
+
+
+#payment choice of student in case if SC/ST scholarship
+
+CITIES=(
+    ('Amritsar','Amritsar'),
+     ('Bathinda','Bathinda'),
+     ('Chandigarh-Mohali-Fatehgarh Sahib','Chandigarh-Mohali-Fatehgarh Sahib'),
+     ('Dehradun','Dehradun'),
+     ('Ghaziabad','Ghaziabad'),
+     ('Haldwani-Bhimtal','Haldwani-Bhimtal'),
+     ('Hamirpur (HP)-Una','Hamirpur (HP)-Una'),
+     ('Jalandhar-Phagwara','Jalandhar-Phagwara'),
+     ('Kurukshetra','Kurukshetra'),
+     ('Ludhiana-Moga','Ludhiana-Moga'),
+     ('Meerut','Meerut'),
+     ('Moradabad','Moradabad'),
+     ('Noida','Noida'),
+     ('Panipat','Panipat'),
+     ('Pathankot','Pathankot'),
+     ('Patiala-Sangrur','Patiala-Sangrur'),
+     ('Roorkee-Muzaffarnagar','Roorkee-Muzaffarnagar'),
+     ('Solan-Shimla','Solan-Shimla'),
+     ('Sirmaur','Sirmaur'),
+     ('Sonipat','Sonipat'),
+     ('Yamunanagar','Yamunanagar'),
+    )
+
