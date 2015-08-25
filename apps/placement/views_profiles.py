ca0ac5329e85d9cd71045ff4cd82a3367c7e2d52
Kartik Patel <kartikpatel1995@gmail.com>
2015-08-25 17:34:15 +0530

    Placement : Educational Details enabled to change

--- apps/placement/views_profiles.py
+++ apps/placement/views_profiles.py
@@ -243,7 +243,8 @@ def educational_details(request):
         return HttpResponseRedirect(reverse('placement.views_profiles.educational_details'))
 
     else :
-      formset = EducationalDetailsFormSet(queryset = EducationalDetails.objects.filter(student = student).exclude(course=previous_sem(student.semester)))
+      #formset = EducationalDetailsFormSet(queryset = EducationalDetails.objects.filter(student = student).exclude(course=previous_sem(student.semester)))
+      formset = EducationalDetailsFormSet(queryset = EducationalDetails.objects.filter(student = student))
     # Override the choices for course as per the course of the user.
     # Added the blank option to make sure that the formset works fine.
     if student.semester[:-2] == 'UG':
@@ -261,18 +262,18 @@ def educational_details(request):
     for form in formset :
       form.fields['discipline'].choices = branches
       form.fields['discipline'].widget = Select(choices = branches )
-    recent = EducationalDetails.objects.filter(student=student, course=previous_sem(student.semester))
-    if recent:
-      recent = recent[0]
-      long_name_course = recent.course
-      for inst in MC.SEMESTER_CHOICES:
-        if inst[0] == previous_sem(student.semester):
-          long_name_course = inst[1]
-          break
-      recent.course = (recent.course, long_name_course)
-
-      long_name_discipline = Branch.objects.get(code=recent.discipline).name
-      recent.discipline = (recent.discipline, long_name_discipline)
+#    recent = EducationalDetails.objects.filter(student=student, course=previous_sem(student.semester))
+#    if recent:
+#      recent = recent[0]
+#      long_name_course = recent.course
+#      for inst in MC.SEMESTER_CHOICES:
+#        if inst[0] == previous_sem(student.semester):
+#          long_name_course = inst[1]
+#          break
+#      recent.course = (recent.course, long_name_course)
+#
+#      long_name_discipline = Branch.objects.get(code=recent.discipline).name
+#      recent.discipline = (recent.discipline, long_name_discipline)
 
     if plac_person.status in ('LCK', 'VRF') :
       # The details are uneditable
@@ -280,7 +281,7 @@ def educational_details(request):
     else :
       template = 'placement/basic_form.html'
     return render_to_response(template , {
-        'recent' : recent,
+#        'recent' : recent,
         'isFormSet' : True,
         'form' : formset,
         'title' : 'Educational Details',
