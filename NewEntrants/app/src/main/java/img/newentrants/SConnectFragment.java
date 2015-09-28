905a022ed3b487ca8bd33d137dff268720c4e0ba
Ankush <ankush.1996@gmail.com>
2015-09-28 23:19:32 +0530

    sconnect fragment left

--- NewEntrants/app/src/main/java/img/newentrants/SConnectFragment.java
+++ NewEntrants/app/src/main/java/img/newentrants/SConnectFragment.java
@@ -4,28 +4,163 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.LayoutInflaterCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.Locale;
 
-public class SConnectFragment extends Fragment {
+
+public class SConnectFragment extends Fragment implements ActionBar.TabListener{
+
+
+    SectionsPagerAdapter mSectionsPagerAdapter;
+    ViewPager mViewPager;
+
     public int position;
-    public SConnectFragment(){
-        position = 0;
+
+    public SConnectFragment(int arg_pos=0) {
+        position = arg_pos;
     }
 
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ){
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
+
+        // For each of the sections in the app, add a tab to the action bar.
+        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText(mSectionsPagerAdapter.getPageTitle(i))
+                            .setTabListener(this));
+        }
+
         View tabView;
-        switch(position){
+        switch (position) {
 
-            case 1: tabView=inflater.inflate(R.layout.fragment_senior_connect_accepted, container, false);
+            case 1:
+                tabView = inflater.inflate(R.layout.fragment_senior_connect_accepted, container, false);
                 break;
-            default: tabView=inflater.inflate(R.layout.fragment_senior_connect_request, container, false);
+            default:
+                tabView = inflater.inflate(R.layout.fragment_senior_connect_request, container, false);
                 break;
         }
+
         return tabView;
+
     }
+
+    public void sendRequest(View view) {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_senior_connect, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Fragment fragment = new SConnectFragment(position);
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            // Show 2 total pages.
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Locale l = Locale.getDefault();
+            switch (position) {
+                case 0:
+                    return getString(R.string.sc1).toUpperCase(l);
+                case 1:
+                    return getString(R.string.sc2).toUpperCase(l);
+            }
+            return null;
+        }
+    }
+
+/*    public class ObjectFragment extends Fragment {
+
+        public int position;
+
+        public ObjectFragment(int arg_pos) {
+            position = arg_pos;
+        }
+
+        public View onCreateView(LayoutInflater inflater,
+                                 ViewGroup container, Bundle savedInstanceState) {
+
+            View tabView;
+            switch (position) {
+
+                case 1:
+                    tabView = inflater.inflate(R.layout.fragment_senior_connect_accepted, container, false);
+                    break;
+                default:
+                    tabView = inflater.inflate(R.layout.fragment_senior_connect_request, container, false);
+                    break;
+            }
+
+            return tabView;
+        }
+    }*/
 }
