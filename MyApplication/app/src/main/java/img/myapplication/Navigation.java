9d5ad84ffa2570e222e470b3d94830a166e006a7
Ankush <ankush.1996@gmail.com>
2015-10-15 20:12:36 +0530

    passing object to activity

--- MyApplication/app/src/main/java/img/myapplication/Navigation.java
+++ MyApplication/app/src/main/java/img/myapplication/Navigation.java
@@ -14,7 +14,7 @@ import android.view.MenuItem;
 public class Navigation extends ActionBarActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
-
+    private User_Model user;
     private NavigationDrawerFragment mNavigationDrawerFragment;
     private int mCurrentPosition;
     private CharSequence mTitle;
@@ -27,7 +27,7 @@ public class Navigation extends ActionBarActivity
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
        // mTitle = getTitle();
-
+        user=(User_Model) getIntent().getSerializableExtra("user");
         mNavigationDrawerFragment.setUp(R.id.navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout));
     }
     public void logout(){
@@ -53,7 +53,7 @@ public class Navigation extends ActionBarActivity
                 break;
             case 5: logout();
                 break;
-            default: fragment=new ProfileFragment();
+            default: fragment=new ProfileFragment(user);
                 break;
         }
        fragmentManager.beginTransaction().replace(R.id.container, fragment).commit();
--- MyApplication/app/src/main/java/img/myapplication/ProfileFragment.java
+++ MyApplication/app/src/main/java/img/myapplication/ProfileFragment.java
@@ -5,14 +5,33 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
 
 
 public class ProfileFragment extends Fragment {
 
-
+    private User_Model profile;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ){
 
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View myView= inflater.inflate(R.layout.fragment_profile, container, false);
+
+        TextView name=(TextView) myView.findViewById(R.id.name);
+        TextView enr=(TextView) myView.findViewById(R.id.enr);
+        //TextView branch=(TextView) getActivity().findViewById(R.id.branch);
+        //TextView state=(TextView) getActivity().findViewById(R.id.state);
+        TextView mobile=(TextView) myView.findViewById(R.id.number);
+        TextView email=(TextView) myView.findViewById(R.id.email);
+
+        name.setText(profile.Name);
+        enr.setText(profile.Enr_No);
+        //branch.setText(profile.Branch);
+        //state.setText(profile.State);
+        mobile.setText(profile.Mobile);
+        email.setText(profile.Email);
+
+        return myView;
+    }
+    public ProfileFragment(User_Model user){
+        profile=user;
     }
 }
