1b9340b76a25347791e7660741161d9df4204e8b
Ankush <ankush.1996@gmail.com>
2015-11-01 22:25:56 +0530

    seniorpage nad peerpage working

--- MyApplication/app/src/main/java/img/myapplication/PeerPage.java
+++ MyApplication/app/src/main/java/img/myapplication/PeerPage.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -23,7 +24,17 @@ public class PeerPage extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_peer_page, container, false);
+        View view= inflater.inflate(R.layout.fragment_peer_page, container, false);
+
+        TextView name= (TextView) view.findViewById(R.id.name);
+        TextView town= (TextView) view.findViewById(R.id.town);
+        TextView state= (TextView) view.findViewById(R.id.state);
+
+        name.setText(peer.name);
+        town.setText(peer.town);
+        state.setText(peer.state);
+
+        return view;
     }
 
 
--- MyApplication/app/src/main/java/img/myapplication/SeniorPage.java
+++ MyApplication/app/src/main/java/img/myapplication/SeniorPage.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -23,7 +24,16 @@ public class SeniorPage extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_senior_page, container, false);
+        View view= inflater.inflate(R.layout.fragment_senior_page, container, false);
+        TextView name= (TextView) view.findViewById(R.id.name);
+        TextView year= (TextView) view.findViewById(R.id.year);
+        TextView branch= (TextView) view.findViewById(R.id.branch);
+
+        name.setText(senior.name);
+        year.setText(senior.year);
+        branch.setText(senior.branch);
+
+        return view;
     }
 
 
