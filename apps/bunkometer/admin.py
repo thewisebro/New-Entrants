97329e2d57180d120831f9a0e3f9cbe58d234952
rachitkewl <rachitkewl@gmail.com>
2016-01-07 23:23:36 +0530

    final_app_done

--- apps/bunkometer/admin.py
+++ apps/bunkometer/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from bunkometer.models import Bunk,TimeTable,Course
+from bunkometer.models import Bunk,TimeTable, Course
 # Register your models here.
 admin.site.register(Bunk)
 admin.site.register(TimeTable)
