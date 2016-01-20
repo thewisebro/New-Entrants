3a79320400f9df78b7729c1e9ac549cf39bc70bb
vaibhav <gvaibhavraj2@gmail.com>
2016-01-21 01:15:24 +0530

    Phd NewUserCreation Script

--- scripts/load_pg.py
+++ scripts/load_pg.py
@@ -7,7 +7,7 @@ def write_row(row_no, row, xl_sheet):
   for i in range(len(row)):
     xl_sheet.write(row_no, i, row[i])
 
-filepath = '../static/excel/PG_1st_year_2015.xlsx'
+filepath = '../static/pg_data.xls'
 xl_book = xlrd.open_workbook(filepath)
 errorFile = xlwt.Workbook()
 errorSheet = errorFile.add_sheet('errors_PG')
@@ -20,30 +20,31 @@ admission_year = 2015
 admission_semester = 'A'
 ok = False
 
-for i in range(1,sheet.nrows):
+for i in range(1,20):
   row = sheet.row_values(i)
   try:
-    enr_no = str(row[0])
+    enr_no = str(row[22])
     name = str(row[1])
-    gender = str(row[5])
-    print row[16]
+    gender = str(row[11])[0]
     try:
-      dob = datetime.datetime(*xlrd.xldate_as_tuple(sheet.cell(i,16).value,xl_book.datemode)).date()
+      dob =  datetime.datetime(*xlrd.xldate_as_tuple(sheet.cell(i,12).value,xl_book.datemode)).date()
+      print dob
     except:
       dob = None
-    nationality = str(row[17])
-    code = str(row[8])
+    nationality = str(row[13])
+    code = str(row[4])
     branch = Branch.objects.get(code = code)
     semester = str(branch.graduation)+'10'
-    contact_no = str(row[15])
+    contact_no = str(row[9])
     if contact_no is None or contact_no is "":
       contact_no = '0'
-    email = str(row[14])
-    address = str(row[9])+str(row[10])+str(row[11])+str(row[12])
+# email = str(row[14])
+    address = str(row[7])
     if len(address)>250:
       address = address[:250]
-    pincode = str(row[13])
+    pincode = str(row[8])
     if ok is False:
+      print "code" + code
       print "Check below values are mapped properly in column else press CTRL+C"
       print "enr_no " + enr_no
       print "name " + name
@@ -53,7 +54,7 @@ for i in range(1,sheet.nrows):
       print "branch " + str(branch)
       print "semester " + str(semester)
       print "contact " + contact_no
-      print "email " + email
+# print "email " + email
       print "address " + address
       print "pincode " + pincode
       time.sleep(10)
@@ -61,7 +62,7 @@ for i in range(1,sheet.nrows):
 
     user = User.objects.filter(username=enr_no)
     if len(user) == 0 and (enr_no is not None and enr_no is not "") and (name is not None and name is not ""):
-      _user = User.objects.create_user(username=enr_no,name=name,password='helloiitr',contact_no=contact_no,birth_date=dob,gender=gender,email=email)
+      _user = User.objects.create_user(username=enr_no,name=name,password='helloiitr',contact_no=contact_no,birth_date=dob,gender=gender)
       group.user_set.add(_user)
       person = Student(user=_user, name=name, semester=semester, semester_no=1, branch=branch, admission_year=admission_year, admission_semtype='A')
       person.save()
@@ -106,4 +107,4 @@ for i in range(1,sheet.nrows):
 
 print "done " + str(done)
 print "error " + str(error)
-errorFile.save('../static/excel/pg_errors.xlsx')
+errorFile.save('../static/pg_errors.xlsx')
